05e391ad158aca434d33a42e057b1e168c09ff8c
sharansharma94 <sharansharma19@gmail.com>
2022-11-24 04:51:35 +0530

    gin server added

---
+++ database/connection.go
@@ -0,0 +1,7 @@
+package database
+
+var Client = ""
+
+func OpenCollection(c string, k string) error {
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"restaurants-manager/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+// var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	// router.Use(middleware.Authentication())
+
+	// routes.Food(router)
+	// routes.Menu(router)
+	// routes.Table(router)
+	// routes.Order(router)
+	// routes.OrderItem(router)
+	// routes.Invoice(router)
+
+	fmt.Println("starting Server...")
+	router.Run(":" + port)
+
+}
