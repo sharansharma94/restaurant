71a66e586a383960f1af41137f6206f0dc531c6d
sharansharma94 <sharansharma19@gmail.com>
2022-11-25 00:15:55 +0530

    dotenv and database added

---
+++ src/database/connection.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client *mongo.Client = dbInstance()
+
+func dbInstance() *mongo.Client {
+	Uri := os.Getenv("MONGO_URI")
+	client, err := mongo.NewClient(options.Client().ApplyURI(Uri))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("connected to mongodb")
+	return client
+
+}
+
+func GetClient() *mongo.Client {
+	return Client
+}
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
+	return collection
+}
