90323b09842572701fbb86433527e9774a43d959
sharansharma94 <sharansharma19@gmail.com>
2022-11-25 01:40:45 +0530

    controller added

--- src/controllers/orderItem.go
+++ src/controllers/orderItem.go
@@ -1,1 +1,22 @@
 package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func GetOrderItems() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func GetOrderItemsByOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func ItemsByOrder(id string) (orderItems []primitive.M, err error) {
+	return nil, nil
+}
+
+func GetOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
